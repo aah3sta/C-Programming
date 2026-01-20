b3ac3d6cfb5cc0c3cd7b4c7a9d67174f379672c1
aah3sta <oyooesther15@gmail.com>
2026-01-20 09:53:36 +0300

    conditionals

---
+++ introduction/conditionals.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+#include <string>
+
+int main(){
+    if (20 > 18){
+        cout << "20 is greater than 18" << "\n";
+    }
+
+    int x = 20;
+    int y = 18;
+    if (x > y) {
+        cout << "x is greater than y";
+    }
+
+    return 0;
+}

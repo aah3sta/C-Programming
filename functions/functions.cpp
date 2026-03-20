b37b2bd8f8f62c7c5ab7b4125253817fb0be7e42
aah3sta <oyooesther15@gmail.com>
2026-03-20 10:03:33 +0300

    functions and parameters

--- functions/functions.cpp
+++ functions/functions.cpp
@@ -6,17 +6,33 @@ void myFunction(){
     cout << "I just got executed!\n";
 }
 
-//parameters - fname is a parameter
-void myFunct(string fname){
-    cout << fname << " Refsnes\n";
+//parameters - fname is a parameter. this has multiple parameters
+void myFunct(string fname, int age){
+    cout << fname << " Oyoo. " << age << " years old. \n";
+}
+
+//default parameter value - executed if no arguement is provided when the function is called
+void countryFunction(string country = "Kenya"){
+    cout << country << "\n";
+}
+
+//return keyword
+int myNumber(int x, int y){
+    return y + x;
 }
 
 int main(){
     myFunction();
     
-    myFunct("Liam");// liam is an arguement
-    myFunct("Jenny");
-    myFunct("Anja");
+    myFunct("Liam", 7);// liam is an arguement
+    myFunct("Jenny", 20);
+    myFunct("Anja", 25);
+
+    countryFunction("Tanzania");
+    countryFunction("Uganda");
+    countryFunction();
     
+    int z = myNumber(3, 4);
+    cout << z;
     return 0;
 }

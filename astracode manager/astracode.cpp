d5b14321a93ba632769e417ad30d528e616920c7
aah3sta <oyooesther15@gmail.com>
2026-03-21 11:06:20 +0300

    added switches

--- astracode manager/astracode.cpp
+++ astracode manager/astracode.cpp
@@ -3,17 +3,14 @@
 #include <vector>
 using namespace std;
 
+//code for header image
 int header_image(){
-    string logo = R"(  /$$$$$$              /$$                         /$$$$$$                  /$$          
- /$$__  $$            | $$                        /$$__  $$                | $$          
-| $$  \ $$  /$$$$$$$ /$$$$$$    /$$$$$$  /$$$$$$ | $$  \__/  /$$$$$$   /$$$$$$$  /$$$$$$ 
-| $$$$$$$$ /$$_____/|_  $$_/   /$$__  $$|____  $$| $$       /$$__  $$ /$$__  $$ /$$__  $$
-| $$__  $$|  $$$$$$   | $$    | $$  \__/ /$$$$$$$| $$      | $$  \ $$| $$  | $$| $$$$$$$$
-| $$  | $$ \____  $$  | $$ /$$| $$      /$$__  $$| $$    $$| $$  | $$| $$  | $$| $$_____/
-| $$  | $$ /$$$$$$$/  |  $$$$/| $$     |  $$$$$$$|  $$$$$$/|  $$$$$$/|  $$$$$$$|  $$$$$$$
-|__/  |__/|_______/    \___/  |__/      \_______/ \______/  \______/  \_______/ \_______/
-                                                                                         
-                                                                                         
+    string logo = R"(   _____            __                _________            .___      
+  /  _  \   _______/  |_____________  \_   ___ \  ____   __| _/____  
+ /  /_\  \ /  ___/\   __\_  __ \__  \ /    \  \/ /  _ \ / __ |/ __ \ 
+/    |    \\___ \  |  |  |  | \// __ \\     \___(  <_> ) /_/ \  ___/ 
+\____|__  /____  > |__|  |__|  (____  /\______  /\____/\____ |\___  >
+        \/     \/                   \/        \/            \/    \/                                                                                          
 )";
     cout << logo << "\n";
     cout << "------------------------------------------------\n";
@@ -21,6 +18,7 @@ int header_image(){
     cout << "------------------------------------------------\n" << "\n";
 }
 
+//adding member details structure and function
 struct member_details{
     string name;
     string university;
@@ -29,7 +27,7 @@ struct member_details{
 
 int add_member(){
     member_details member;
-    cout << "Kindly enter your details.\n";
+    cout << "Kindly enter member details.\n";
     cout << "Enter Full Name: ";
     cin >> member.name;
 
@@ -39,6 +37,47 @@ int add_member(){
     cout << "University Name: ";
     cin >> member.university;
 }
+
+//exiting the program
+int close_program(){
+    cout << "Thank you for choosing AstraCode!\n";
+    EXIT_SUCCESS;
+}
+
+//code that executes when app runs
+int user_options(){
+    //give user options.
+    cout << R"(WELCOME TO THE ASTRACODE COMMUNITY MANAGEMENT SYSTEM!
+    Kindly select a number to proceed.
+    1. Add a member
+    2. Display member details
+    3. Search member
+    4. Delete member
+    5. Exit
+    )";
+
+    int user_choice;
+    cin >> user_choice;
+
+    if (user_choice >= 1 && user_choice <=5){
+        cout << "Selection successful!\n";
+        switch (user_choice)
+        {
+        case 1:
+            add_member();
+            break;
+        case 5:
+            close_program();
+            break;
+        default:
+            break;
+        }
+    } else {
+        cout << "Enter a valid number!\n ";
+        user_options();
+    }
+}
+
 
 /*
 int display_members(){
@@ -51,7 +90,10 @@ int search_member(){
 */
  
 int main(){
+    //Display astracodeimage
     header_image();
-    add_member();
+    //display user options and run program
+    user_options();
+
     return 0;
 }
